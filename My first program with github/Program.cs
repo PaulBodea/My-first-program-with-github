5822655ab10fd378e5779e964d9dfda7674f5f21
Paul Bodea <bodea.paul97@yahoo.com>
2020-07-13 09:21:20 +0300

    Add project files.

---
+++ My first program with github/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace My_first_program_with_github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
